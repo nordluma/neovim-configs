8f4da3c6dbfdbaf6e9e6a1c6deb7f9be04a0c698
nordluma <maxnordlund.95@hotmail.com>
2023-06-15 21:52:18 +0300

    build: setup autotag

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -10,6 +10,10 @@ require("nvim-treesitter.configs").setup({
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
+  autotag = {
+      enable = true,
+  },
+
   highlight = {
     enable = true,
 
@@ -20,3 +24,4 @@ require("nvim-treesitter.configs").setup({
     additional_vim_regex_highlighting = false,
   },
 })
+
