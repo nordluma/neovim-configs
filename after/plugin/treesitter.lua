4f3565e82753a532226765ea920c6825285257bb
nordluma <maxnordlund.95@hotmail.com>
2024-04-11 22:17:53 +0300

    chore: remove `vimdoc`

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup({
     -- A list of parser names, or "all" (the five listed parsers should always
     -- be installed
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python", "markdown", "rust" },
+    ensure_installed = { "c", "lua", "vim", "query", "javascript", "typescript", "python", "markdown", "rust" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
