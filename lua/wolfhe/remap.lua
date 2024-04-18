38649026e7ce5f9700d12afae25b4e5c192f5f21
nordluma <maxnordlund.95@hotmail.com>
2024-04-18 23:00:37 +0300

    fix(remap): jump to `lazy.lua` instead of `packer.lua`

--- lua/wolfhe/remap.lua
+++ lua/wolfhe/remap.lua
@@ -7,7 +7,6 @@ vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
--- No work :(
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
@@ -19,5 +18,5 @@ vim.keymap.set("n", "<leader>dd", "\"_dd")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- Quickjump to packer
-vim.keymap.set("n", "<leader>jvp", "<cmd>e ~/.config/nvim/lua/wolfhe/packer.lua<CR>")
+-- Quickjump to `lazy.lua`
+vim.keymap.set("n", "<leader>jvp", "<cmd>e ~/.config/nvim/lua/wolfhe/lazy.lua<CR>")
