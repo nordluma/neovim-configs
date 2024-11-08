5479933cb03109383089a9c92a32014af7cb6637
nordluma <maxnordlund.95@hotmail.com>
2024-11-08 21:06:58 +0200

    chore(treesitter): typo

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -22,7 +22,7 @@ require("nvim-treesitter.configs").setup({
         -- time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for
         -- indentation).
-        -- Usin this option may slow down your editor, and you may see some
+        -- Using this option may slow down your editor, and you may see some
         -- duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
