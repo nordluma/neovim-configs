c1bc94d55a71e4ffdf6c906b1fe7f5c3133b9d19
nordluma <maxnordlund.95@hotmail.com>
2025-02-17 23:58:07 +0200

    feat(telescope): add keybind to select env for `rest.nvim`

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,5 @@
+require("telescope").load_extension("rest")
+
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
@@ -5,4 +7,7 @@ vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>re', function()
+    require("telescope").extensions.rest.select_env()
 end)
