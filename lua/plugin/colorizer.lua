b3dc4a73d7516007cb23cdc91468b2682cb9951e
nordluma <maxnordlund.95@hotmail.com>
2025-09-05 18:42:39 +0300

    feat(colorizer): don't colorize in `vim` files

--- lua/plugin/colorizer.lua
+++ lua/plugin/colorizer.lua
@@ -1,11 +1,12 @@
 return {
-  "norcalli/nvim-colorizer.lua",
-  event = "VeryLazy",
-  config = function()
-    require("colorizer").setup({
-      "*",
-      css = { rgb_fn = true },
-      html = { names = false }
-    })
-  end
+	"norcalli/nvim-colorizer.lua",
+	event = "VeryLazy",
+	config = function()
+		require("colorizer").setup({
+			"*",
+			"!vim",
+			css = { rgb_fn = true },
+			html = { names = false },
+		})
+	end,
 }
