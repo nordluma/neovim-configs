8d898250e8b93db352bae8a644308d59cf89815f
nordluma <maxnordlund.95@hotmail.com>
2025-09-05 18:48:42 +0300

    fix(render-markdown): typo

--- lua/plugin/render-markdown.lua
+++ lua/plugin/render-markdown.lua
@@ -2,7 +2,7 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
-		"nvim-tree/nvim-wev-devicons",
+		"nvim-tree/nvim-web-devicons",
 	},
 	lazy = false,
 	---@module "render-markdown"
