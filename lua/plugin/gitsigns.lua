a7d537b69878ad8e463f86eff5a47680cb008fc5
nordluma <maxnordlund.95@hotmail.com>
2025-09-25 00:32:43 +0300

    feat(gitsigns): add configurations and keymaps

--- lua/plugin/gitsigns.lua
+++ lua/plugin/gitsigns.lua
@@ -9,5 +9,29 @@ return {
 			topdelete = { text = "➤" },
 			changedelete = { text = "▎" },
 		},
+		signs_staged = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signs_staged_enable = true,
+		numhl = true,
+		on_attach = function(bufnr)
+			local gitsigns = require("gitsigns")
+			local set_keymap = function(mode, l, r, opts)
+				opts = opts or {}
+				opts.buffer = bufnr
+				vim.keymap.set(mode, l, r, opts)
+			end
+
+			set_keymap("n", "<leader>hb", function()
+				gitsigns.blame_line({ full = true })
+			end)
+
+			set_keymap("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+		end,
 	},
 }
