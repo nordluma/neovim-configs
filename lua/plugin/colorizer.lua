05a18e85d81756e768235900abd6c6fb27defe52
nordluma <maxnordlund.95@hotmail.com>
2025-12-11 22:47:47 +0200

    style(colorizer): move options to `opts`

--- lua/plugin/colorizer.lua
+++ lua/plugin/colorizer.lua
@@ -1,12 +1,10 @@
 return {
 	"norcalli/nvim-colorizer.lua",
 	event = "VeryLazy",
-	config = function()
-		require("colorizer").setup({
-			"*",
-			"!vim",
-			css = { rgb_fn = true },
-			html = { names = false },
-		})
-	end,
+	opts = {
+		"*",
+		"!vim",
+		css = { rgb_fn = true },
+		html = { names = false },
+	},
 }
