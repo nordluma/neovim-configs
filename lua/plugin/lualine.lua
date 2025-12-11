98cf2432cf32ec3eb293ef6e20300068b4040c05
nordluma <maxnordlund.95@hotmail.com>
2025-12-11 22:40:54 +0200

    style(lualine): use gitsigns as diff source

--- lua/plugin/lualine.lua
+++ lua/plugin/lualine.lua
@@ -1,72 +1,50 @@
+local lazy = require("lazy.status")
+local function diff_source()
+	local gitsigns = vim.b.gitsigns_status_dict
+	if gitsigns then
+		return {
+			added = gitsigns.added,
+			modified = gitsigns.changed,
+			removed = gitsigns.removed,
+		}
+	end
+end
+
 return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		lazy = false,
-		config = function()
-			local lazy_status = require("lazy.status")
-			require("lualine").setup({
-				options = {
-					--
-					--theme = 'palenight',
-					theme = "tokyonight-night",
-					disabled_filetypes = {},
+		opts = {
+			--theme = 'palenight',
+			options = { theme = "tokyonight-night" },
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					"branch",
+					{ "diff", source = diff_source },
+					{ "diagnostics", sources = { "nvim_workspace_diagnostic" } },
 				},
-				sections = {
-					lualine_a = { "mode" },
-					lualine_b = {
-						"branch",
-						"diff",
-						{
-							"diagnostics",
-							sources = { "nvim_diagnostic" },
-							symbols = { error = "E ", warn = "W ", hint = "H ", info = "I " },
-						},
-					},
-					lualine_c = {
-						{
-							"filename",
-							file_status = true,
-							path = 0,
-						},
-					},
-					lualine_x = {
-						{
-							lazy_status.updates,
-							cond = lazy_status.has_updates,
-							color = { fg = "#ff9e64" },
-						},
-						{ "encoding" },
-						{ "fileformat" },
-						{ "filetype" },
-					},
-					lualine_y = {
-						"progress",
-						"selectioncount",
-						"location",
-					},
-					lualine_z = {
-						--{ 'datetime', style = "%H.%M" }
-						{ "lsp_status", ignore_lsp = { "typos_lsp", "emmet_language_server" } },
-					},
+				lualine_c = { { "filename", file_status = true, path = 1 } },
+				lualine_x = {
+					{ lazy.updates, cond = lazy.has_updates, color = { fg = "#ff9e64" } },
+					"encoding",
+					"fileformat",
+					"filetype",
 				},
-				inactive_sections = {
-					lualine_a = {},
-					lualine_b = {},
-					lualine_c = {
-						{
-							"filename",
-							file_status = true,
-							path = 0,
-						},
-					},
-					lualine_x = { "location" },
-					lualine_y = {},
-					lualine_z = {},
+				lualine_y = { "progress", "selectioncount", "location" },
+				lualine_z = {
+					{ "lsp_status", ignore_lsp = { "typos_lsp", "emmet_language_server" } },
 				},
-				tabline = {},
-				extensions = { "fugitive" },
-			})
-		end,
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = { { "filename", file_status = true, path = 0 } },
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { "location" },
+				lualine_z = {},
+			},
+		},
 	},
 }
