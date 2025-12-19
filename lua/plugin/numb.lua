7e92bbe66347033de05469174cdfdfd1b7e19cf0
nordluma <max@suomenseniorihoiva.fi>
2025-12-19 12:03:01 +0200

    feat(plugin): install `numb`

---
+++ lua/plugin/numb.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"nacro90/numb.nvim",
+		event = "VeryLazy",
+		-- lazy = false,
+		opts = {
+			show_numbers = true,
+			show_cursorline = true,
+			hide_relativenumbers = true,
+			centered_peeking = true,
+		},
+	},
+}
