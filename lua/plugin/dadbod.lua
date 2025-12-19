c9a8eb7badc11691c66b2ef4261e22e90999a787
nordluma <max@suomenseniorihoiva.fi>
2025-12-19 12:04:11 +0200

    style(plugin/dadbod): formatting

--- lua/plugin/dadbod.lua
+++ lua/plugin/dadbod.lua
@@ -1,18 +1,22 @@
 return {
-  {
-    'kristijanhusak/vim-dadbod-ui',
-    dependencies = {
-      { 'tpope/vim-dadbod' },
-      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'plsql' } }
-    },
-    cmd = {
-      'DBUI',
-      'DBUIToggle',
-      'DBUIAddConn',
-      'DBUIFindBuffer'
-    },
-    init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-    end
-  },
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			{ "tpope/vim-dadbod", lazy = true },
+			{
+				"kristijanhusak/vim-dadbod-completion",
+				ft = { "sql", "mysql", "plsql" },
+				lazy = true,
+			},
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConn",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
 }
